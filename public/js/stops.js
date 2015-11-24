238013151449bc8271bccab642ac186543d22f42
Kuan Butts <kuanbutts@yahoo.com>
2015-11-23 20:16:06 -0500

    toggle capabilities for variables + auto resizer

--- public/js/stops.js
+++ public/js/stops.js
@@ -7,13 +7,15 @@ var globals = {
 	lastcall: {
 		date: null,
 		hour: null,
-		res: null
+		res: null,
+		vis: null
 	}
 }
 
 function plotStops (res) {
 	res.forEach(function (stop) {
 		var s = L.circle([stop.stop_lat, stop.stop_lon], 2, {color: "#00c3c3", weight: 1});
+		s.properties = stop;
 		globals.stops.all.push(s);
 	});
 	redrawStops();
@@ -68,12 +70,11 @@ function toggleStops (argument) {
 };
 
 function query () {
-	toggleLoad("on");
-
 	var date = $("#param_date")[0].value;
 	var hour = $("#param_hour")[0].value.split(":")[0];
 
 	if (date !== globals.lastcall.date || hour !== globals.lastcall.hour) {
+		toggleLoad("on");
 		$.ajax({
 		  url: "api/arrivals",
 		  type: "get",
@@ -83,7 +84,6 @@ function query () {
 		  	globals.lastcall.hour = hour;
 		  	globals.lastcall.res = res;
 
-		  	console.log(res);
 		  	renderQuery();
 		  	toggleLoad("off");
 		  },
@@ -91,21 +91,124 @@ function query () {
 		    alert("Error: ", err);
 		  }
 		});
+	} else {
+		renderQuery();
 	}
 };
 
 function renderQuery () {
-	var temp = {};
-	Object.keys(globals.lastcall.res).forEach(function (stop_id) {
-		var stop = globals.lastcall.res[stop_id];
-		if (stop[0] !== null && stop[1] !== null) {
-			console.log("Wow");
+	if (globals.lastcall.vis !== null) {
+		Object.keys(globals.lastcall.vis).forEach(function (k) {
+			Object.keys(globals.lastcall.vis[k]).forEach(function (a) {
+				map.removeLayer(globals.lastcall.vis[k][a]);
+			});
+		});
+		globals.lastcall.vis = null;
+	}
+
+	var res = globals.lastcall.res;
+	var circle_scale = $("#param_scale")[0].value;
+	var toVis = {};
+
+	globals.stops.all.forEach(function (stop) {
+		var id = stop.properties.stop_id;
+		if (res[id] !== undefined) {
+			var props = {
+				early: 0,
+				late: 0,
+				ontime: 0
+			};
+
+			Object.keys(res[id]).forEach(function (dir) {
+				if (res[id][dir] !== null) {
+					var a = res[id][dir];
+
+					props.early += getCounts("early", a);
+					props.late += getCounts("late", a);
+					props.ontime += a.on_time;
+				}
+			});
+
+			if (toVis[id] == undefined) toVis[id] = {0: null, 1: null};
+			toVis[id] = {}
+			var ll = [stop.properties.stop_lat, stop.properties.stop_lon]
+
+			toVis[id].early = L.circle(ll, props.early*circle_scale, {color: "blue", weight: 1});
+			toVis[id].early["properties"] = {val: props.early, vis: true};
+
+			toVis[id].late = L.circle(ll, props.late*circle_scale, {color: "red", weight: 1});
+			toVis[id].late["properties"] = {val: props.late, vis: true};
+
+			toVis[id].ontime = L.circle(ll, props.ontime*circle_scale, {color: "yellow", weight: 1});
+			toVis[id].ontime["properties"] = {val: props.ontime, vis: true};
 		}
 	});
-	globals.stops.all.forEach(function (stop) {
-		
+
+	Object.keys(toVis).forEach(function (stop_id) {
+		var stop = toVis[stop_id];
+		stop.early.addTo(map)
+		stop.late.addTo(map)
+		stop.ontime.addTo(map)
 	});
-}
+
+	globals.lastcall.vis = toVis;
+};
+
+function resizeCircles () {
+	var circle_scale = $("#param_scale")[0].value;
+	if (globals.lastcall.vis !== null) {
+		Object.keys(globals.lastcall.vis).forEach(function (k) {
+			Object.keys(globals.lastcall.vis[k]).forEach(function (a) {
+				var r = Number(globals.lastcall.vis[k][a].properties.val) * circle_scale;
+				globals.lastcall.vis[k][a].setRadius(r);
+			});
+		});
+	}
+};
+
+function toggleStopTypes () {
+  var early = $("#param_stoptype_early")[0].checked;
+  var ontime = $("#param_stoptype_ontime")[0].checked;
+  var late = $("#param_stoptype_late")[0].checked;
+
+	if (globals.lastcall.vis !== null) {
+		Object.keys(globals.lastcall.vis).forEach(function (k) {
+			Object.keys(globals.lastcall.vis[k]).forEach(function (a) {
+				var add_early = ((a == "early") && early)
+				var rem_early = ((a == "early") && !early)
+
+				var add_ontime = ((a == "ontime") && ontime)
+				var rem_ontime = ((a == "ontime") && !ontime)
+
+				var add_late = ((a == "late") && late)
+				var rem_late = ((a == "late") && !late)
+
+				if (add_early || add_ontime || add_late) {
+					if (globals.lastcall.vis[k][a].properties.vis == false) {
+						globals.lastcall.vis[k][a].addTo(map);
+					}
+					globals.lastcall.vis[k][a].properties.vis = true;
+				} else if (rem_early || rem_ontime || rem_late) {
+					if (globals.lastcall.vis[k][a].properties.vis == true) {
+						map.removeLayer(globals.lastcall.vis[k][a]);
+					}
+					globals.lastcall.vis[k][a].properties.vis = false;
+				}
+			});
+		});
+	}
+};
+
+function getCounts (aspect, stop) {
+	var base = 0;
+	var wanted = getCategories()
+	Object.keys(wanted).forEach(function (k) {
+		if (wanted[k] && k.indexOf(aspect) > -1) {
+			base += stop[k];
+		}
+	});
+	return base;
+};
 
 function toggleLoad (specific) {
 	if (specific == "on")
